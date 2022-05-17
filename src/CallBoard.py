1164374be7d9993c09432cbfd1708af53d5d9ad1
Braeden Smith <braedensmith629@gmail.com>
2022-05-17 13:38:11 -0500

    Created a basic layout and file structure

--- src/CallBoard.py
+++ src/CallBoard.py
@@ -1,16 +1,29 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import * # will limit once I know what I'm using
+import Header, Body, Footer
 
+class MainWindowLayout(QVBoxLayout):
+    def __init__(self):
+        super().__init__()
+        header = Header.Header()
+        body = Body.Body()
+        footer = Footer.Footer()
 
-def main():
-    app = QApplication(sys.argv)
+        self.addLayout(header)
+        self.addLayout(body)
+        self.addLayout(footer)
 
-    window = QWidget()
-    window.setWindowTitle('Simple')
-    window.show()
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Test")
+        self.resize(1920, 1080)
 
-    sys.exit(app.exec_())
-
+        mainWindowLayout = MainWindowLayout()
+        self.setLayout(mainWindowLayout)
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
