b3baa1c8640d2b11fdb0053bf3bb1e56c2f6405a
Braeden Smith <braedensmith629@gmail.com>
2022-05-21 12:52:03 -0500

    Added update functionality to snowData

--- src/ui/snowData.py
+++ src/ui/snowData.py
@@ -6,36 +6,61 @@ class Active(QVBoxLayout):
         super().__init__()
 
         incidentTimesLayout = QVBoxLayout()
-        incidentTimesLayout.addWidget(QLabel(str(1) + " ≥ 4 hrs"))
-        incidentTimesLayout.addWidget(QLabel(str(3) + " ≥ 2 hrs"))
-        incidentTimesLayout.addWidget(QLabel(str(2) + " < 2 hrs"))
+        self.over4 = QLabel("x ≥ 4 hrs")
+        incidentTimesLayout.addWidget(self.over4)
+        self.over2 = QLabel("x ≥ 2 hrs")
+        incidentTimesLayout.addWidget(self.over2)
+        self.under2 = QLabel("x < 2 hrs")
+        incidentTimesLayout.addWidget(self.under2)
 
         hLayout = QHBoxLayout()
-        hLayout.addWidget(QLabel("16"))
+        self.activeTotal = QLabel("x")
+        hLayout.addWidget(self.activeTotal)
         hLayout.addLayout(incidentTimesLayout)
 
         self.addWidget(QLabel("Active Incidents"))
         self.addLayout(hLayout)
+    
+    def updateIncidents(self, over4, over2, under2):
+        self.over4.setText(str(over4) + " ≥ 4 hrs")
+        self.over2.setText(str(over2) + " ≥ 2 hrs")
+        self.under2.setText(str(under2) + " < 2 hrs")
+
+        self.activeTotal.setText(str(over4 + over2 + under2))
+
+
 
 class OnHoldTriage(QHBoxLayout):
     def __init__(self):
         super().__init__()
         
-        onHold = QVBoxLayout()
-        onHold.addWidget(QLabel("On Hold"))
-        onHold.addWidget(QLabel("12"))
+        onHoldLayout = QVBoxLayout()
+        onHoldLayout.addWidget(QLabel("On Hold"))
+        self.onHold = QLabel("x")
+        onHoldLayout.addWidget(self.onHold)
 
-        triage = QVBoxLayout()
-        triage.addWidget(QLabel("Triage"))
-        triage.addWidget(QLabel("8"))
+        triageLayout = QVBoxLayout()
+        triageLayout.addWidget(QLabel("Triage"))
+        self.triage = QLabel("x")
+        triageLayout.addWidget(self.triage)
 
-        self.addLayout(onHold)
-        self.addLayout(triage)
+        self.addLayout(onHoldLayout)
+        self.addLayout(triageLayout)
+
+    def updateIncidents(self, onHold, triage):
+        self.onHold.setText(str(onHold))
+        self.triage.setText(str(triage))
 
 class ServiceNowData(QVBoxLayout):
     def __init__(self):
         super().__init__()
         
         self.addWidget(QLabel("ServiceNow"))
-        self.addLayout(Active())
-        self.addLayout(OnHoldTriage())
+        self.activeLayout = Active()
+        self.addLayout(self.activeLayout)
+        self.ohtLayout = OnHoldTriage()
+        self.addLayout(self.ohtLayout)
+
+    def updateIncidents(self, over4, over2, under2, onHold, triage):
+        self.activeLayout.updateIncidents(over4, over2, under2)
+        self.ohtLayout.updateIncidents(onHold, triage)
